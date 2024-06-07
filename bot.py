472680cec3fa6aaf83bb1aaf7caf22a4eb00e370
takogonia <timurkogoniia2205@gmail.com>
2024-06-07 18:38:46 +0300

    25%

--- bot.py
+++ bot.py
@@ -1,1 +1,27 @@
-print("hello")
+import telebot
+from bs4 import BeautifulSoup
+import requests
+
+
+TOKEN = '***********************'
+bot = telebot.TeleBot(TOKEN)
+
+
+def internet_search(msg):
+    # еще не придумал
+    url = f'https:/www.google.com/search?q={msg}'
+
+
+@bot.message_handler(commands=['start', 'help'])
+def start_help_message(message):
+    bot.reply_to(message, 'Привет, я поисковой бот, отправь мне запрос и я найду по нему информацию в интернете')
+
+
+@bot.message_handler(func=lambda message: True)
+def get_and_reply(message):
+    msg = message.text
+    reply = internet_search(msg)
+    bot.reply_to(message, reply)
+
+
+bot.infinity_polling()
